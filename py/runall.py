fa64c26f83b96e49aeca95c71ef435bbfe1fa680
Heinrich Mellmann <mellmann@informatik.hu-berlin.de>
2016-03-23 17:22:58 +0100

    generate only not existing labels run everything in parallell

--- py/runall.py
+++ py/runall.py
@@ -1,17 +1,38 @@
 #!/usr/bin/python
 import os, sys, getopt
+from multiprocessing import Process
 import mainExportKicks
+
+def run_stuff(logFile, outFile):
+  print(logFile)
+  print(outFile)
+  cache = mainExportKicks.init(logFile)
+  mainExportKicks.run(cache, outFile)
 
 if __name__ == "__main__":
 
   rootDir = "../log"
+  
+  max_number_of_processes = 4
+  threads = []
   
   for root, dirs, files in os.walk(rootDir):
     for file in files:
         if file.endswith(".log"):
           logFile = os.path.join(root, file)
           outFile = os.path.join(root, "labels.json")
-          print(logFile)
-          print(outFile)
-          cache = mainExportKicks.init(logFile)
-          mainExportKicks.run(cache, outFile)
+          
+          if os.path.isfile(outFile):
+            print "EXISTS ", outFile
+            continue
+          
+          try:
+             p = Process(target=run_stuff, args=(logFile, outFile, ))
+             threads.append(p)
+             p.start()
+          except:
+             print "Error: unable to start thread"
+             
+             
+  for t in threads:
+    t.join()
