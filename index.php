3ecea99f0e4a0c84b333d806366c893aeb4562b1
Benjamin Schlotter <benji@schlotterfam.de>
2018-07-26 15:51:43 +0200

    Revert "Merge branch 'feature/pyVideoConverter'"
    This reverts commit e3ae5ed3140ad5965c7927c08731686a14624034, reversing
    changes made to 03e0ddcaa7cd03778eaf19e2b12807951e4982ec.
    
    except the python file

--- index.php
+++ index.php
@@ -1,15 +1,4 @@
 <?php
-// uncomment/delete in production mode or set to FALSE
-defined('APP_DEBUG') or define('APP_DEBUG', TRUE);
-
-require(__DIR__ . '/php/autoload.php');
-require(__DIR__ . '/php/base/Application.php');
-
-$config = require(__DIR__ . '/php/config.php');
-
-(new app\Application($config))->run();
-
-/*    
   include 'php/listcontents.php';
   
   $game = NULL;
@@ -39,4 +28,3 @@ if( $game != NULL) {
   include 'php/overview_template.php';
 }
 ?>
-*/
