fefdccd8fc7418f1574e806268cbaec2e7186c03
Philipp Strobel <philippstrobel@posteo.de>
2018-09-28 11:42:14 +0200

    adjusted configuration file and added ability to create/dump the default configuration; also multiple log directories are now possible

--- VideoLogLabeling/index.php
+++ VideoLogLabeling/index.php
@@ -1,28 +1,43 @@
 <?php
 // required includes
+require_once 'php/Config.php';
 require_once 'php/Event.php';
 
+// was the script called by commandline?
+if (PHP_SAPI === 'cli') {
+    if($argv[1] === 'dumpconfig') {
+        echo Config::dump();
+    } else {
+        echo "ERROR: Unknown argument!\n";
+    }
+    exit(0);
+}
+
 // global variables
-$logPath = './log';
 $basepath = __DIR__;
 $name = isset($_GET["name"]) ? $_GET["name"] : "new";
 $game = NULL;
 $events = [];
 $errors = [];
 
-// read log directory
-try {
-    foreach(new DirectoryIterator($logPath) as $file) {
-        if (!$file->isDot() && $file->isDir()) {
-            $event = new \Event($file);
-            if ($event->isValid()) {
-                $events[] = $event;
+// read log directories
+foreach (Config::paths() as $path) {
+    if (is_dir($path) && is_readable($path)) {
+        try {
+            foreach(new DirectoryIterator($path) as $file) {
+                if (!$file->isDot() && $file->isDir()) {
+                    $event = new \Event($file);
+                    if ($event->isValid()) {
+                        $events[] = $event;
+                    }
+                }
             }
+        } catch (Exception $e) {
+            $errors[] = 'reading log directory: ' . $e->getMessage();
         }
     }
-} catch (Exception $e) {
-    $errors[] = 'reading log directory: ' . $e->getMessage();
 }
+
 usort($events, function ($a, $b) { return $a->getDate() < $b->getDate(); });
 
 // search for a requested game
--- VideoLogLabeling/php/Config.php
+++ VideoLogLabeling/php/Config.php
@@ -2,32 +2,76 @@
 
 class Config
 {
+    // the singleton instance variable
     private static $instance = null;
 
-    // configuration for events
-    private $event = [];
-    // configuration for games
-    private $game = [];
-    // configuration for logs
-    private $log = [];
+    // directories of the events/log files
+    private $paths = [ './log' ];
 
+    // default configuration for events
+    private $event = [
+        "regex" => "(\\d{4}-\\d{2}-\\d{2})_(\\w+)"
+    ];
+
+    // default configuration for games
+    private $game = [
+        "regex" => "(\\d{4}-\\d{2}-\\d{2}_\\d{2}-\\d{2}-\\d{2})_(\\S+)_vs_(\\S+)_half([1,2])",
+        "dirs"  => [
+            "nao"   => "game_logs",
+            "gc"    => "gc_logs",
+            "video" => "videos",
+            "data"  => "extracted"
+        ],
+        "video_types" => [ "mp4" ]
+    ];
+
+    // default configuration for logs
+    private $log = [
+        "regex" => "(\\d{1})_(\\d{2})_(\\w+)",
+        "name" => "game.log",
+        "sync" => "game.log.videoanalyzer.properties",
+        "labels" => ["labels", ".json"]
+    ];
+
+    /**
+     * Private constructor for the singleton pattern.
+     * Reads the config file and initializes the config variables.
+     */
     private function __construct() {
-        $config = json_decode(file_get_contents('config'), true);
+        // if config file exists, read contents and set available fields.
+        if (is_file('config')) {
+            $config = json_decode(file_get_contents('config'), true);
 
-        foreach (get_class_vars(__CLASS__) as $k => $v) {
-            if(array_key_exists($k, $config)) {
-                $this->$k = $config[$k];
+            foreach (get_class_vars(__CLASS__) as $k => $v) {
+                if($config && array_key_exists($k, $config)) {
+                    $this->$k = $config[$k];
+                }
             }
         }
     }
 
-    private function __clone() { /* disable copy */ }
+    /**
+     * Prevent copying this class - it's a singleton!
+     */
+    private function __clone() {}
 
+    /**
+     * Returns the singleton instance of this class.
+     * @return Config
+     */
     final public static function getInstance() {
         if (self::$instance === null) {
             self::$instance = new Config();
         }
         return self::$instance;
+    }
+
+    /**
+     * Shorthand function for accessing the log paths configuration
+     * @return array
+     */
+    public static function paths() {
+        return self::getInstance()->paths;
     }
 
     /**
@@ -57,4 +101,17 @@ class Config
         return self::getInstance()->log[$key];
     }
 
+    /**
+     * Returns the current configuration as JSON string.
+     * This can be used to create a default configuration file ("config") and adjust the parts necessary for further usage.
+     * @return string
+     */
+    public static function dump() {
+        return json_encode([
+            'paths' => self::getInstance()->paths,
+            'event' => self::getInstance()->event,
+            'game' => self::getInstance()->game,
+            'log' => self::getInstance()->log,
+        ], JSON_PRETTY_PRINT);
+    }
 }
