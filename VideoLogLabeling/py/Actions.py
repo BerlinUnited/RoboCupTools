e2d3c950d43be51c6a6fc7d140cd8090d8af572e
Philipp Strobel <philippstrobel@posteo.de>
2018-10-02 18:54:02 +0200

    attempt to make it easier to extend

---
+++ VideoLogLabeling/py/Actions.py
@@ -0,0 +1,32 @@
+
+# option in o and o[option]["state"].name == state
+
+def turn(symbols, option):
+    return 'turn_to_attack_direction' in option
+
+def kick_right(symbols, option):
+    return 'sidekick' in option and option['sidekick']['state'].name == 'sidekick_left_foot'
+
+def kick_left(symbols, option):
+    return 'sidekick' in option and option['sidekick']['state'].name == 'sidekick_right_foot'
+
+def kick_short(symbols, option):
+    return 'fast_forward_kick' in option
+
+def kick_long(symbols, option):
+    return 'kick_with_foot' in option
+
+def sidekick_right(symbols, option):
+    return 'path_striker2018' in option and option['path_striker2018']['state'].name == 'sidekick_right'
+
+def sidekick_left(symbols, option):
+    return 'path_striker2018' in option and option['path_striker2018']['state'].name == 'sidekick_left'
+
+def forwardkick_left(symbols, option):
+    return 'path_striker2018' in option and option['path_striker2018']['state'].name == 'forwardkick_left'
+
+def forwardkick_right(symbols, option):
+    return 'path_striker2018' in option and option['path_striker2018']['state'].name == 'forwardkick_right'
+
+def fallen(symbols, option):
+    return 'fall_down_and_stand_up' in option and option['fall_down_and_stand_up']['state'].name == 'stand_up'
--- VideoLogLabeling/py/main.py
+++ VideoLogLabeling/py/main.py
@@ -2,7 +2,9 @@
 import os
 import re
 
+import Actions
 from utils import config, Event
+
 
 def read_logs(root):
     events = []
@@ -16,18 +18,34 @@ def read_logs(root):
 
     return events
 
+def load_actions():
+    actions = {}
+
+    for a in dir(Actions):
+        if not a.startswith('_'):
+            actions[a] = getattr(Actions, a)
+
+    return actions
+
 if __name__ == "__main__":
     log_dir = '../log'
 
     events = read_logs(log_dir)
     print(events)
 
+    actions = load_actions()
+    print(actions)
+
     for e in events:
         for g in e.games:
             for l in g.logs.values():
+                # check if the syncing infos with the video exists
                 if not l.has_syncing_file():
                     print e, '/', g, '/', l, '- missing syncing file! creating default ...'
                     l.create_default_syncing_file()
+                # check if the default label file exits
                 if not l.has_label_file():
                     print e, '/', g, '/', l, '- missing label file! creating default ...'
-
+                # check if all actions were parsed
+                if [a for a in actions.keys() if a not in l.parsed_actions()]:
+                    l.create_label_file(actions)
